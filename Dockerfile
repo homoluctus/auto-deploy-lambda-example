06855e8db95957132a611d7b48692696599745cf
homoluctus <w.slife18sy@gmail.com>
2019-09-01 13:25:09 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:10.16-alpine
+WORKDIR /app
+CMD ["/bin/ash"]
---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { IncomingWebhook } = require('@slack/webhook');
+
+const post = async (event) => {
+  try {
+    const url = process.env.SLACK_WEBHOOK || '';
+
+    if (url === '') {
+      throw new Error(`
+        Error: Missing SLACK_WEBHOOK
+        Please set SLACK_WEBHOOK as environment variable
+      `);
+    }
+
+    const webhook = new IncomingWebhook(url);
+    const msg = {
+      text: 'Auto Deploy to Lambda with GitHub Actions'
+    };
+    const res = await webhook.send(msg);
+    console.log(res);
+
+    return;
+  } catch (err) {
+    console.error(err);
+  }
+}
